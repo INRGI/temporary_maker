93625b8a9ae7690b6f8c04aed3d13a9b1e8f8563
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-03-27 13:16:28 +0100

    add link to excel

--- apps/frontend/src/pages/Home/Home.styled.tsx
+++ apps/frontend/src/pages/Home/Home.styled.tsx
@@ -9,3 +9,20 @@ export const Container = styled.div`
   background-image: linear-gradient(to bottom right, #1e1e1e, #3a3a3a);
   height: calc(100vh - 40px);
 `;
+
+export const TextNotification = styled.p`
+  color: #f5f5f5;
+  font-size: 1.25rem;
+  margin: 0;
+  padding: 0;
+  padding-left: 20px;
+`;
+
+export const LinkNotification = styled.a`
+  color: #ff0000;
+  font-size: 1.25rem;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  text-decoration: underline;
+`;
--- apps/frontend/src/pages/Home/Home.tsx
+++ apps/frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import PresetContainer from "../../components/PresetContainer/PresetContainer";
-import { Container } from "./Home.styled";
+import { Container, LinkNotification, TextNotification } from "./Home.styled";
 
 const Home: React.FC = () => {
   return (
     <Container>
      <PresetContainer />
+     <TextNotification>Copies not found? Add missed ones <LinkNotification target="_blank" href="https://docs.google.com/spreadsheets/d/1kJT1_YLuDKwYcwpxTMm38JClRUH3f7XPY0P4MUhEPNk/edit?gid=0#gid=0">here</LinkNotification></TextNotification>
     </Container>
   );
 };
