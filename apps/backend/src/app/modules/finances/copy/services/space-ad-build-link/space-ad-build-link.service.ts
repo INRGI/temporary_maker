5f46a4b09b2a334c9d1e317dbd384e0916868425
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-07-04 13:55:58 +0200

    [fix] link building issue

--- apps/backend/src/app/modules/finances/copy/services/space-ad-build-link/space-ad-build-link.service.ts
+++ apps/backend/src/app/modules/finances/copy/services/space-ad-build-link/space-ad-build-link.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { SpaceAdBuildLinkPayload } from "./space-ad-build-link.payload";
 import { BuildLinkService } from "../build-link/build-link.service";
+import { GetMondayDataService } from "../get-monday-data/get-monday-data.service";
 
 @Injectable()
 export class SpaceAdBuildLinkService {
-  constructor(private readonly buildLinkService: BuildLinkService) {}
+  constructor(
+    private readonly buildLinkService: BuildLinkService,
+    private readonly getMondayDataService: GetMondayDataService
+  ) {}
 
   public async buildLink(payload: SpaceAdBuildLinkPayload): Promise<string> {
     const { copyName, linkUrl } = payload;
@@ -17,10 +21,35 @@ export class SpaceAdBuildLinkService {
       ? copyName.match(/\d+([a-zA-Z].*)/)[1]
       : "";
 
+    let trackingData: {
+      product: string;
+      trackingData: string;
+      imgData: string;
+      isForValidation: boolean;
+    };
+
+    trackingData = await this.getMondayDataService.getRedtrackData({
+      product,
+      trackingType: linkUrl && linkUrl.trackingType ? linkUrl.trackingType : "",
+    });
+
+    if (!trackingData) {
+      trackingData = await this.getMondayDataService.getRedtrackData({
+        product: `*${product}`,
+        trackingType:
+          linkUrl && linkUrl.trackingType ? linkUrl.trackingType : "",
+      });
+
+      if (!trackingData || !trackingData.trackingData) {
+        return "urlhere";
+      }
+    }
+
     const link = await this.buildLinkService.buildLink({
       product,
       productLift,
       productImage,
+      trackingData,
       linkUrl: linkUrl,
     });
 
