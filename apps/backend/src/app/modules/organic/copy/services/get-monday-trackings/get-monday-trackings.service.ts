21a6dd6f08fe12a7d22492d8a3fe28eaaabc84ec
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-09-02 14:22:29 +0200

    [fix] ignore Date column name

--- apps/backend/src/app/modules/organic/copy/services/get-monday-trackings/get-monday-trackings.service.ts
+++ apps/backend/src/app/modules/organic/copy/services/get-monday-trackings/get-monday-trackings.service.ts
@@ -1,10 +1,9 @@
-
-import { GetMondayTrackingsResponseDto } from '@epc-services/interface-adapters';
+import { GetMondayTrackingsResponseDto } from "@epc-services/interface-adapters";
 import {
   InjectMondayApiService,
   MondayApiServicePort,
-} from '@epc-services/monday-api';
-import { Injectable, Logger } from '@nestjs/common';
+} from "@epc-services/monday-api";
+import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class GetMondayTrackingsService {
@@ -19,28 +18,29 @@ export class GetMondayTrackingsService {
 
   public async geTrackings(): Promise<GetMondayTrackingsResponseDto> {
     const mondayData = await this.mondayApiService.getProductData(
-      'OGHBC',
+      "OGHBC",
       2013547302
     );
 
     if (!mondayData.length) {
-      throw new Error('Redtracks not found');
+      throw new Error("Redtracks not found");
     }
 
     const trackings: string[] = mondayData[0].column_values
       .map((column) => {
         if (
-          column.column.title !== 'Subitems' &&
-          column.column.title !== 'Status' &&
-          column.column.title !== 'B\\B' &&
-          column.column.title !== 'Sector' &&
-          column.column.title !== 'Bad Copies' &&
-          column.column.title !== '(B)Broadcast Copies' &&
-          column.column.title !== 'Domain Sending' &&
-          column.column.title !== 'Copy Location' &&
-          column.column.title !== 'Expires' &&
-          column.column.title !== 'WarmUp' &&
-          column.column.title !== 'Abbreviation'
+          column.column.title !== "Subitems" &&
+          column.column.title !== "Status" &&
+          column.column.title !== "B\\B" &&
+          column.column.title !== "Sector" &&
+          column.column.title !== "Bad Copies" &&
+          column.column.title !== "(B)Broadcast Copies" &&
+          column.column.title !== "Domain Sending" &&
+          column.column.title !== "Copy Location" &&
+          column.column.title !== "Expires" &&
+          column.column.title !== "WarmUp" &&
+          column.column.title !== "Abbreviation" &&
+          column.column.title !== "Date"
         ) {
           return column.column.title;
         }
